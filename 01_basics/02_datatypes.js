9cbb7bb0f4e81a6f32361f83d7508119dea29927
RiyaKrishna <riyakrishna425@gmail.com>
2025-09-18 16:32:36 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; // treat all js code as newer version/code
+//alert('hello')
+//alert(3+3) we are using node.js,not browser
+
+console.log(3
+    +
+    3) // code readability should be high 
+console.log("Riya")
