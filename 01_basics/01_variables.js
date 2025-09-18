98b74945ea3ed27421fc03f9488acc80e1d5449b
RiyaKrishna <riyakrishna425@gmail.com>
2025-09-18 16:19:28 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId =144553
+let accountEmail="riya@facebook.com"
+var accountPassword ="12345"
+accountCity ="Jaipur"
+let accountState;
+
+//accountId = 2 // not allowed
+/* Prefer not to use var 
+because of issue in block scope and functional scope */
+accountEmail="riya123@gmail.com"
+accountPassword= 167857
+accountCity="Delhi"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
