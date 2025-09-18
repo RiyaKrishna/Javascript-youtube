47fe09013a3b4125b008f92ca88d166c8f56bc64
RiyaKrishna <riyakrishna425@gmail.com>
2025-09-18 18:14:51 +0000

    conversion of datatypes

---
+++ 01_basics/conversionoroperation.js
@@ -0,0 +1,50 @@
+let score = 33
+let score1="33"
+let score2 = null
+let score3= undefined
+let score4 = true
+
+
+console.log(typeof score);
+console.log(typeof(score));
+console.log(typeof score1);
+
+let valueInNumber = Number(score1)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(score2);
+console.log(score3);
+console.log(score4);
+
+valueInNumber = Number(score2)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+ valueInNumber = Number(score3)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+ valueInNumber = Number(score4)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc"=>NaN
+// true =>1, false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// 1 => true; 0=> false
+//"" => false
+//"Riya"=> true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber))
